c946c40413e050a73d7bfbe27f47144e1bdb54de
Michael <m24927605@gmail.com>
2018-06-03 03:17:57 +0800

    delete fakeData

--- ESS/fakeData/products.js
+++
@@ -1,8 +0,0 @@
-var product_data = [{
-  "product_id": "1",
-  "product_type": "針織衫",
-  "product_name": "顯廋百搭針織衫針織衫",
-  "product_price": 100,
-  "product_inventory_counts": 20,
-  "product_detail": {}
-}]
